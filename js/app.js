3d156a1d79d4eed9042c380b34177d049e0982ff
rk <rebeccakruk@outlook.com>
2022-11-24 20:00:57 -0700

    working random generator

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 function activitySuccess(response){
     let data = response.data;
     console.log(data);
-    for(let ideas of data){
-    document.body.insertAdjacentHTML(`afterbegin`, `<h2>${ideas[`key`]}</h2>`);
-}};
-
-//     let data = response.data;
-    // for(let post of data){
+    // for(let result of data){
+    document.body.insertAdjacentHTML(`afterbegin`,
+     `<h2>Activity: ${data[`activity`]}</h2>`);
+    document.body.insertAdjacentHTML(`afterbegin`,
+     `<h3>Type: ${data[`type`]}</h3>`);
+};
 
 function activityFailure(error){
     console.log(`loser`);
-}
+};
 
+function clickRandom(){
 axios.request({
     url: "http://www.boredapi.com/api/activity/"
 }).then(activitySuccess).catch(activityFailure);
-
-document.getElementById(`randomActivity`).addEventListener(`click`, activitySuccess);
+};
+document.getElementById(`randomActivity`).addEventListener(`click`, clickRandom);
